16d7bc3fa3e630093511af66080419b51d38db8a
UvaishaKhan <uvaisha.khan@goolean.tech>
2024-09-30 17:47:05 +0530

    code formated

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     return "This is the about page."
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
     return "Contact information: 123 Main St., City, State, ZIP"
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html', name='Flask')
-@app.route('/home')
+    return render_template("index.html", name="Flask")
+
+
+@app.route("/home")
 def home2():
-    return "This is the home page."    
+    return "This is the home page."
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
